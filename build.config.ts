4d87c06cf934511c977c0de689a988db71083990
Alexander Nortung <alex_nortung@live.dk>
2023-09-22 16:13:09 +0200

    build: created a single build instead of two

--- build.config.ts
+++ build.config.ts
@@ -2,16 +2,9 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig([
   {
-    entries: ['src/index'],
+    entries: ['src/index', 'src/cli/index'],
     declaration: true,
     clean: true,
-    rollup: {
-      emitCJS: true,
-    },
-  },
-  {
-    entries: ['src/cli/index'],
-    outDir: 'bin',
     rollup: {
       emitCJS: true,
     },
