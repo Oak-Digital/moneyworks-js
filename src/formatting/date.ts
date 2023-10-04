d54a48e2a390fa01a15b24bc0644ee4b7aa9f6af
Alexander Nortung <alexander.nortung@oakdigital.dk>
2023-10-04 17:04:29 +0200

    fix(formatting): formatDate will now pad zeros

--- src/formatting/date.ts
+++ src/formatting/date.ts
@@ -1,3 +1,6 @@
 export function formatDate(date: Date) {
-  return `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}`
+  const year = date.getFullYear()
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+  return `${year}${month}${day}`
 }
